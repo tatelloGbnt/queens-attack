ea59a65c199ce440f28cb00c71456e6b0561906c
tatello <tatellog@gmail.com>
2020-04-03 15:54:23 -0600

    first commit

---
+++ src/components/BoardComponent/BoardComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const BoardComponent = () => <div className="Board">
+  <div>
+    <p>Im the board</p>
+  </div>
+</div>
+
+BoardComponent.displayName = "BoardComponent"
+
+export default BoardComponent
