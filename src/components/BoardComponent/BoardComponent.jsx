b6c6f89fcfd664ff843012461230d248a7bf2ed8
tatelloGbnt <tania.tello@globant.com>
2020-04-06 13:34:08 -0500

    Stylus was configured

--- src/components/BoardComponent/BoardComponent.jsx
+++ src/components/BoardComponent/BoardComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import styles from './_BoardComponent.styl'
 
-export const BoardComponent = () => <div className="Board">
+export const BoardComponent = () => <div className="styles.board">
   <div>
     <p>Im the board</p>
   </div>
